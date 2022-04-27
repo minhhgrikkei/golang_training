afbe89672bb8d50c6da1db3aeee7cf2f61c8d8c0
minhhgrikkei <104499577+minhhgrikkei@users.noreply.github.com>
2022-04-27 17:43:30 +0700

    Add files via upload

---
+++ cards/main.go
@@ -0,0 +1,34 @@
+package main
+
+func main() {
+	/*var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	card = newCard()
+	fmt.Println(card) */
+
+	/*
+		cards := []string{newCard(), "Ace of Diamonds"}
+		cards = append(cards, "Six of Spades")
+		for i, card := range cards {
+			fmt.Println(i, card)
+		}*/
+	/*
+		cards := newDeck()
+		hand, remainingCard := deal(cards, 5)
+
+		hand.print()
+		remainingCard.print()
+	*/
+
+	/*
+		cards := newDeck()
+		fmt.Println(cards.toString())
+	*/
+	cards := newDeck()
+	cards.saveToFile("mycards")
+}
+
+/*
+func newCard() string {
+	return "Five of Diamonds"
+}*/
